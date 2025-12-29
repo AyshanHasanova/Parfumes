d5c85023ff34a202040158d7c7f3c855b3b950a5
unknown <ahsnova557@gmail.com>
2025-12-29 17:27:23 +0400

    fix: base urls updated

--- frontend/src/redux/api/productsApi.js
+++ frontend/src/redux/api/productsApi.js
@@ -1,77 +1,21 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-// export const productApi = createApi({
-//   reducerPath: 'productApi',
-//   baseQuery: fetchBaseQuery({ baseUrl: '/api/v1', credentials: 'include' }),
-//   tagTypes: ['Products'],
-//   endpoints: builder => ({
-//     createProduct: builder.mutation({
-//       query: formData => ({
-//         url: '/admin/product',
-//         method: 'POST',
-//         body: formData,
-//       }),
-//       invalidatesTags: ['Products']
-//     }),
-//     updateProduct: builder.mutation({
-//       query: ({ id, formData }) => ({
-//         url: `/admin/product/${id}`,
-//         method: 'PUT',
-//         body: formData,
-//       }),
-//       invalidatesTags: ['Products']
-//     }),
-//     deleteProduct: builder.mutation({
-//       query: id => ({
-//         url: `/admin/product/${id}`,
-//         method: 'DELETE',
-//       }),
-//       invalidatesTags: ['Products']
-//     }),
-//     getProducts: builder.query({
-//       query: () => '/mehsullar',
-//       providesTags: ['Products']
-//     }),
-//     getProductsDetails :builder.query({
-//          query: (id) => `/mehsullar/${id}`,
-//       providesTags: ['Products']
-//     })
-//   })
-// })
-
-// export const {
-//   useCreateProductMutation,
-//   useUpdateProductMutation,
-//   useDeleteProductMutation,
-//   useGetProductsQuery,
-//   useGetProductsDetailsQuery
-// } = productApi 
-
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productApi = createApi({
   reducerPath: 'productApi',
-  // Netlify-da işləməsi üçün mütləq tam URL (https://...) yazılmalıdır
   baseQuery: fetchBaseQuery({ 
     baseUrl: 'https://parfumes-1.onrender.com/api/v1', 
     credentials: 'include' 
   }),
   tagTypes: ['Products'],
   endpoints: (builder) => ({
-    // Bütün məhsulları gətirmək üçün
     getProducts: builder.query({
       query: () => '/mehsullar',
       providesTags: ['Products']
     }),
-
-    // Məhsulun detallarını gətirmək üçün
     getProductsDetails: builder.query({
       query: (id) => `/mehsullar/${id}`,
       providesTags: ['Products']
     }),
-
-    // Yeni məhsul yaratmaq (Admin)
     createProduct: builder.mutation({
       query: (formData) => ({
         url: '/admin/product',
@@ -80,8 +24,6 @@ export const productApi = createApi({
       }),
       invalidatesTags: ['Products']
     }),
-
-    // Məhsulu yeniləmək (Admin)
     updateProduct: builder.mutation({
       query: ({ id, formData }) => ({
         url: `/admin/product/${id}`,
@@ -90,8 +32,6 @@ export const productApi = createApi({
       }),
       invalidatesTags: ['Products']
     }),
-
-    // Məhsulu silmək (Admin)
     deleteProduct: builder.mutation({
       query: (id) => ({
         url: `/admin/product/${id}`,
